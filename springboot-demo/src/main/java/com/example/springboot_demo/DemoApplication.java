fe8bf3a10183ac4682d8ed41a59f3c62bcfba368
Vince Miller <m.vincent83@gmail.com>
2025-10-06 18:09:09 -0500

    cli runner, admin seed

--- springboot-demo/src/main/java/com/example/springboot_demo/DemoApplication.java
+++ springboot-demo/src/main/java/com/example/springboot_demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.springboot_demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,18 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return (args) -> {
+			if (userRepository.findByUsername("admin").isEmpty()) {
+				User admin = new User();
+				admin.setUsername("admin");
+				admin.setPassword(passwordEncoder.encode("admin"));
+				admin.setRoles("ROLE_ADMIN");
+				userRepository.save(admin);
+			}
+		};
+	}
+
 }
+
